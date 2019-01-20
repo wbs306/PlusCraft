0edf8293553c073b88c44ea16b021579d653dd2a
wbs306 <wbs306@gmail.com>
2019-01-20 20:29:54 +0800

    subscribe more block events

--- src/main/java/com/wbs/pluscraft/event/BlockEventPublish.java
+++ src/main/java/com/wbs/pluscraft/event/BlockEventPublish.java
@@ -23,4 +23,31 @@ public class BlockEventPublish {
 		blockJson.addProperty("player", event.getPlayer().getName());
 		AdapterPublisher.publishJson(blockJson);
 	}
+
+	@SubscribeEvent
+	public void onBlockNotifiedByNeighbor(BlockEvent.NeighborNotifyEvent event) throws IOException{
+		JsonObject blockJson = EventJson.getBlockJson("notified", event);
+		blockJson.addProperty("side", event.getNotifiedSides().toString());
+		AdapterPublisher.publishJson(blockJson);
+	}
+
+	@SubscribeEvent
+	public void onCropGrow(BlockEvent.CropGrowEvent.Pre event) throws IOException {
+		AdapterPublisher.publishJson(EventJson.getBlockJson("crop_grow", event));
+	}
+
+	@SubscribeEvent
+	public void onCropGrowSuccessfully(BlockEvent.CropGrowEvent.Post event) throws IOException {
+		JsonObject blockJson = EventJson.getBlockJson("crop_grow_successfully", event);
+		blockJson.addProperty("origin", event.getOriginalState().getBlock().getUnlocalizedName());
+		AdapterPublisher.publishJson(blockJson);
+	}
+
+	@SubscribeEvent
+	public void onFarmlandTrample(BlockEvent.FarmlandTrampleEvent event) throws IOException {
+		JsonObject blockJson = EventJson.getBlockJson("farmland_trample", event);
+		blockJson.addProperty("entity_name", event.getEntity().getName());
+		blockJson.addProperty("height", event.getFallDistance());
+		AdapterPublisher.publishJson(blockJson);
+	}
 }
